c091f5ff403e34c3fae13b18679372e37e4133ae
Annalise Xiao <annalisexiao@Annalises-MacBook-Air-4.local>
2025-04-17 12:32:57 -0500

    CUSTOMER.sql first draft

---
+++ CUSTOMER.sql
@@ -0,0 +1,9 @@
+CREATE TABLE CUSTOMER (
+    CustomerID INT PRIMARY KEY,
+    FirstName VARCHAR(50), 
+    LastName VARCHAR(50),
+    EmailAddresses VARCHAR(100),
+    PhoneNumber VARCHAR(10),
+    BillingAddress VARCHAR(300),
+)
+
