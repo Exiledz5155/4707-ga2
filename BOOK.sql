2d6a3b9c46884052b66e545c42bf4440de45ae9d
Annalise Xiao <annalisexiao@Annalises-MacBook-Air-4.local>
2025-04-19 15:55:56 -0500

    author change

--- BOOK.sql
+++ BOOK.sql
@@ -2,10 +2,11 @@ CREATE TABLE BOOK (
     BookID INT PRIMARY KEY,
     Title VARCHAR(300),
     Genre VARCHAR(50),
-    Price DECIMAL (10, 2)
+    Price DECIMAL (10, 2),
     PublicationYear YEAR, 
     ContactInfo VARCHAR(100),
     Name VARCHAR(200),
     StockQuantity INT,
-    FOREIGN KEY (AAuthorID) REFERENCES AUTHOR(Author(ID)),
-)
+    AAuthorID INT,
+    FOREIGN KEY (AAuthorID) REFERENCES AUTHOR(AuthorID)
+);
