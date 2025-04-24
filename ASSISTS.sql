112a45e9e0b400d78336ec41635990244bcd1220
Annalise Xiao <annalisexiao@Annalises-MacBook-Air-4.local>
2025-04-24 13:44:48 -0500

    updated assists

--- ASSISTS.sql
+++ ASSISTS.sql
@@ -1,7 +1,7 @@
 CREATE TABLE ASSISTS (
     CCustomerID INT,
     EEmployeeID INT,
-    PRIMARY KEY ((CCustomerID, EEmployeeID)), 
+    PRIMARY KEY (CCustomerID, EEmployeeID), 
     FOREIGN KEY (CCustomerID) REFERENCES CUSTOMER(CustomerID),
     FOREIGN KEY (EEmployeeID) REFERENCES EMPLOYEE(EmployeeID)
     -- use composite key for avoiding duplicate entries?
