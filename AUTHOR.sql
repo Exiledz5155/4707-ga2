b34d52ec993bf6256227e898f466bfe3fd1efdb1
Annalise Xiao <annalisexiao@Annalises-MacBook-Air-4.local>
2025-04-27 20:57:38 -0500

    updated author

--- AUTHOR.sql
+++ AUTHOR.sql
@@ -1,8 +1,5 @@
 CREATE TABLE AUTHOR (
     AuthorID INT PRIMARY KEY,
     FirstName VARCHAR(100),
-    LastName VARCHAR(100)
-    -- FOREIGN KEY (BBookID) REFERENCES BOOK(BookID),
-    -- not sure if we need this line bc alr have in book.sql
-    -- one author can write many books so i think just having the book reference it shld be ok
+    LastName VARCHAR(100),
 );
